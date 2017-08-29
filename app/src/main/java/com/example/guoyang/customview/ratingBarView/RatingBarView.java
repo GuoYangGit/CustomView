0ec7be08582a03c0dc9239b14a5a48c07687d693
guoyang <guoyang_add@163.com>
2017-08-29 21:47:48 +0800

    评分view

--- app/src/main/java/com/example/guoyang/customview/ratingBarView/RatingBarView.java
+++ app/src/main/java/com/example/guoyang/customview/ratingBarView/RatingBarView.java
@@ -12,13 +12,19 @@ import android.view.View;
 
 import com.example.guoyang.customview.R;
 
+
 /**
  * Created by guoyang on 2017/8/29.
  */
 
 public class RatingBarView extends View {
+    //选中前后的图片
     private Bitmap mStarNormalBitmap, mStarFousBitmap;
+    //每个图片之间的间隙
+    private int mBarPadding;
+    //总分数
     private int mGradeNumber = 5;
+    //当前分数
     private int mCurrentGrade = 0;
 
 
@@ -32,17 +38,23 @@ public class RatingBarView extends View {
 
     public RatingBarView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        initPaint(context, attrs);
+    }
 
+    private void initPaint(Context context, AttributeSet attrs) {
         TypedArray array = context.obtainStyledAttributes(attrs, R.styleable.RatingBarView);
-        int starNormalId = array.getResourceId(R.styleable.RatingBarView_starNormal, 0);
-        int starFocusId = array.getResourceId(R.styleable.RatingBarView_starFocus, 0);
+        int starNormalId = array.getResourceId(R.styleable.RatingBarView_ratingBarStarNormal, 0);
+        int starFocusId = array.getResourceId(R.styleable.RatingBarView_ratingBarStarFocus, 0);
+        mBarPadding = (int) array.getDimension(R.styleable.RatingBarView_ratingBarPadding, 10);
         if (starNormalId == 0) {
             throw new RuntimeException("请设置属性 starNormal");
         }
         if (starFocusId == 0) {
             throw new RuntimeException("请设置属性 starFocus");
         }
+        //设置选中图片
         mStarFousBitmap = BitmapFactory.decodeResource(getResources(), starFocusId);
+        //设置默认图片
         mStarNormalBitmap = BitmapFactory.decodeResource(getResources(), starNormalId);
         array.recycle();
     }
@@ -51,18 +63,20 @@ public class RatingBarView extends View {
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         int height = mStarNormalBitmap.getHeight();
-        int width = mStarNormalBitmap.getWidth() * mGradeNumber;
+        int width = (mStarNormalBitmap.getWidth() + mBarPadding) * mGradeNumber;
         setMeasuredDimension(width, height);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         for (int i = 0; i < mGradeNumber; i++) {
-            int x = i * mStarNormalBitmap.getWidth();
+            int x = i * (mStarNormalBitmap.getWidth() + mBarPadding);
             int top = getPaddingTop();
-            if (mCurrentGrade>i){
+            if (mCurrentGrade > i) {
+                //绘制评分
                 canvas.drawBitmap(mStarFousBitmap, x, top, null);
-            }else {
+            } else {
+                //绘制默认
                 canvas.drawBitmap(mStarNormalBitmap, x, top, null);
             }
         }
@@ -73,13 +87,14 @@ public class RatingBarView extends View {
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
             case MotionEvent.ACTION_MOVE:
-
-                float moveX = event.getX();//获取手指当前按下x的位置
+                //获取手指当前按下x的位置
+                float moveX = event.getX();
                 //event.getRawX()获取屏幕X的位置
-                int currentGrade = (int) (moveX/mStarNormalBitmap.getWidth()+1);
-                if (currentGrade<0) currentGrade = 0;
-                if (currentGrade>mGradeNumber) currentGrade = mGradeNumber;
-                if (currentGrade == mCurrentGrade){
+                //根据用户当前位置计算出当前评分
+                int currentGrade = (int) (moveX / (mStarNormalBitmap.getWidth()+mBarPadding) + 1);
+                if (currentGrade < 0) currentGrade = 0;
+                if (currentGrade > mGradeNumber) currentGrade = mGradeNumber;
+                if (currentGrade == mCurrentGrade) {
                     return true;
                 }
                 mCurrentGrade = currentGrade;
